5dc62e7db441f5cfe9abe552c8b559ffaa4038fc
Samrudhi Santaji <samrudhi.santaji@team.telstra.com>
2022-01-06 09:30:36 +0530

    Renamed the controller

--- AssessmentDemo/Supporting/SceneDelegate.swift
+++ AssessmentDemo/Supporting/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
          guard let windowScene = (scene as? UIWindowScene) else { return }
-   let navController = UINavigationController(rootViewController: DemoTableViewController())
+   let navController = UINavigationController(rootViewController: DataTableViewController())
         navController.navigationBar.prefersLargeTitles = true
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
